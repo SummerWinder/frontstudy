bcd3e33991b4b6cd14a0c7acc83553b3f5df60aa
SummerWinder <458277748@qq.com>
2023-08-01 14:32:06 +0800

    修改首页布局

--- java_script/01_js.js
+++ java_script/01_js.js
@@ -1,28 +1,32 @@
 /* 2022-10-13 15:30 author by zjc02 */
 var person = {firstName:"John", lastName:"Doe", age:50, eyeColor:"blue"};
 
-
+// 从html文件id为first的标签中获取数字，并返回
 function get_first_num()
     {
     var num1=document.getElementById("first").value
     return num1;
     }
 
+// 从html文件id为second的标签中获取数字，并返回
 function get_second_num()
     {
     var num2=document.getElementById("second").value
     return num2;
     }
 
+// 这是两个数字相加的方法
 function add()
     {
-        var a = get_first_num();
+//        var a = get_first_num();
+        var a = document.getElementById("first").value;
         var b = get_second_num();
         var re =Number(a)+Number(b);
 //        var re =get_first_num() + get_second_num();直接写变量没有存储起来，所以从输入框获取的数字就无法进行计算了
         send_result("计算结果是：" + re);
     }
 
+// 这是两个数字相减的方法
 function subtraction()
     {
         var a = get_first_num();
@@ -31,7 +35,7 @@ function subtraction()
         send_result("计算结果是：" + re);
     }
 
-//这里的函数中加了一个变量，将最终结果写入到html文件中
+//这里的函数中加了一个变量，将最终结果写入到html文件中id为result的标签
 function send_result(lastResult)
     {
     var num=document.getElementById("result")
@@ -53,7 +57,9 @@ function myFunction1(name,job)
 
 function sum(num1,num2)
 {
+    var re= num1 * num2
 	alert(num1 * num2);
+	send_result("计算结果是：" + re);
 }
 
 
